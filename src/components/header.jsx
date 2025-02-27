69ee687000bba9d23cf303a055bf0ac3619039a7
safwan <ahnafaudri2084@gmail.com>
2025-02-27 19:50:51 +0600

    auth-method

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { Button } from "./ui/button";
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton,
+} from "@clerk/clerk-react";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <>
+      <nav className=" px-4 py-4 flex justify-between items-center">
+        <Link>
+          <img src="./comp-logo.png" className="h-10" />
+        </Link>
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </nav>
+    </>
+  );
 };
 
 export default Header;
