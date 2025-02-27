30fa104681c365f54d842688b81b7f8dff6d573a
safwan <ahnafaudri2084@gmail.com>
2025-02-27 19:08:37 +0600

    logo & footer

--- src/layouts/app-layout.jsx
+++ src/layouts/app-layout.jsx
@@ -1,10 +1,16 @@
 import { Outlet } from "react-router-dom";
+import Header from "../components/header";
+import Footer from "../components/footer";
 
 const AppLayout = () => {
   return (
     <div>
       <div className="background"></div>
-      <Outlet />
+      <main className="min-h-screen">
+        <Header />
+        <Outlet />
+      </main>
+      <Footer />
     </div>
   );
 };
